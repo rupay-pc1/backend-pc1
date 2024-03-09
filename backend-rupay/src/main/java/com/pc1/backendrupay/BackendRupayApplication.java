a17668ebc97ab62a805cd251fe689047def24f00
Davyson <davysonguimares74@gmail.com>
2024-03-09 07:48:46 -0300

    Inserindo projeto no repositório.

---
+++ backend-rupay/src/main/java/com/pc1/backendrupay/BackendRupayApplication.java
@@ -0,0 +1,13 @@
+package com.pc1.backendrupay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRupayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRupayApplication.class, args);
+	}
+
+}
---
+++ backend-rupay/src/test/java/com/pc1/backendrupay/BackendRupayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc1.backendrupay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRupayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
